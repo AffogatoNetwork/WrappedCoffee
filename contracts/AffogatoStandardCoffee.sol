9417c0401951d467bd6d87723a94d5137b980edd
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2019-09-04 15:19:41 -0600

    Fixes AffogatoStandardCoffee to extend ERC20Detailed only

--- contracts/AffogatoStandardCoffee.sol
+++ contracts/AffogatoStandardCoffee.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.5.9;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "./interfaces/IAffogatoToken.sol";
 
-contract AffogatoStandardCoffee is ERC20, ERC20Detailed {
+contract AffogatoStandardCoffee is ERC20Detailed, IAffogatoToken {
 
     address public CoffeeTokenHolderAddress;
 
@@ -12,15 +13,15 @@ contract AffogatoStandardCoffee is ERC20, ERC20Detailed {
         _;
     }
 
-    constructor(address _CoffeeTokenHolderAddress)  ERC20Detailed("Affogato Standard Coffee", "ASC", 0) public {
+    constructor(address _CoffeeTokenHolderAddress) ERC20Detailed("Affogato Standard Coffee", "ASC", 0) public {
         CoffeeTokenHolderAddress = _CoffeeTokenHolderAddress;
     }
 
-    function wrapCoffee(address _owner, uint _amount) public onlyCoffeeHolder {
+    function wrapCoffee(address _owner, uint _amount) external onlyCoffeeHolder {
         _mint(_owner, _amount);
     }
 
-    function unwrapCoffee(address _owner, uint _amount) public onlyCoffeeHolder {
+    function unwrapCoffee(address _owner, uint _amount) external onlyCoffeeHolder {
          _burn(_owner, _amount);
     }
 }
